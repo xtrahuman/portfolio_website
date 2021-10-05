fc9f0c086b95bcd0ebdce5f2c2c920b19261ef75
unknown <okpara_tochukwu@yahoo.com>
2021-10-05 13:20:31 +0100

    update: styling add addition of javacript manipulation in mobile menu

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-const menubar = document.querySelector("#mobile_menu");
-const navlink = document.querySelector(".nav-flex");
-const mode = document.querySelector(".modal-pager");
+const menubar = document.querySelector('#mobile_menu');
+const navlink = document.querySelector('.nav-flex');
 
-// window.addEventListener("DOMContentLoaded", modalpage);
-
-// const modalpage = () => {
-//   mode.innerhtml = ``;
-// };
-
-window.onclick = function (event) {
-  if (!event.target.matches(".vector")) {
-    navlink.classList.remove("toggle");
-    menubar.classList.remove("toggle-menu");
+window.onclick = function check(event) {
+  if (!event.target.matches('.vector')) {
+    navlink.classList.remove('toggle');
+    menubar.classList.remove('toggle-menu');
   }
 };
 
-//Display mobile menu
+// Display mobile menu
+
 const mobilemenu = () => {
-  menubar.classList.toggle("toggle-menu");
-  navlink.classList.toggle("toggle");
+  menubar.classList.toggle('toggle-menu');
+  navlink.classList.toggle('toggle');
 };
 
-menubar.addEventListener("click", mobilemenu);
+menubar.addEventListener('click', mobilemenu);
