81bc8f8d18a66053f52aebf155144a6c29878733
xtrahuman <okpara_tochukwu@yahoo.com>
2022-02-12 12:30:05 +0100

    fixed linter errors

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const PageContent = [{
   title: 'Art Gallery',
   img: '/public/art-gallery1.PNG',
   contentText:
-      "This is a react web app that displays the list of art works gotten from art institute of chicago API, with the search option you can search the artwork, the artworks gotten through this api changes at every interval, kindly click on the link below to checkout the project",
+      'This is a react web app that displays the list of art works gotten from art institute of chicago API, with the search option you can search the artwork, the artworks gotten through this api changes at every interval, kindly click on the link below to checkout the project',
   skills1: 'css3',
   skills2: 'React',
   skills3: 'JavaScript',
@@ -72,7 +72,7 @@ const PageContent = [{
   title: 'Space Travelers',
   img: '/public/space-travelers2.PNG',
   contentText:
-      "This is a single page application built with react where I worked with real live data from the SpaceX API. My task was to build a web application for a company that provides commercial and scientific space travel services. The application allows users to book rockets and dragons, and join selected space missions. each booked rockets/dragons and joined missions can be viewed in the profile section, kindly click the link below to view project",
+      'This is a single page application built with react where I worked with real live data from the SpaceX API. My task was to build a web application for a company that provides commercial and scientific space travel services. The application allows users to book rockets and dragons, and join selected space missions. each booked rockets/dragons and joined missions can be viewed in the profile section, kindly click the link below to view project',
   skills1: 'css3',
   skills2: 'React',
   skills3: 'Redux',
@@ -83,7 +83,7 @@ const PageContent = [{
   title: 'Movie Time',
   img: './public/javascript-capstone2.PNG',
   contentText:
-      "Movie time is a capstone project built mainly with JavaScript, the project is about displaying a list of movies where users can like or write a comment to any of the movies, all likes and comments are stored in an interactive api, Kindly click the links below to view the project",
+      'Movie time is a capstone project built mainly with JavaScript, the project is about displaying a list of movies where users can like or write a comment to any of the movies, all likes and comments are stored in an interactive api, Kindly click the links below to view the project',
   skills1: 'css3',
   skills2: 'html',
   skills3: 'Webpack',
